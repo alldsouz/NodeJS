c6fb0ff3aa3a14beb176800609b16f862e55a301
alldsouz <97952084+alldsouz@users.noreply.github.com>
2022-06-23 14:50:54 +0530

    2:40PM

---
+++ directory.js
@@ -0,0 +1,15 @@
+var fs = requires('fs');
+
+// //make new directory
+// fs.mkdirSync('stuff');
+
+// //delete new directory
+// fs.rmdirSync('stuff');
+
+// //async will do something once the function has been completed
+// fs.mkdir('stuff', function(){
+//     fs.readFile('readme.txt', 'utf8', function(err, data){
+//         fs.writeFile('./stuff/writeme.txt', data);
+//     });
+// });
+
