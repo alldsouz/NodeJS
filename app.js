23092866e59422b77605bab80ec5ae7582f49a33
alldsouz <97952084+alldsouz@users.noreply.github.com>
2022-06-25 15:43:02 +0530

    3:40PM

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('this works')
+var express = require('express');
+
+var todoController = require('./controllers/todoController');
+
+var app = express();
+
+//set up template engine 
+app.set('view engine' ,'ejs');
+
+//staticfiles 
+app.use('/assets', express.static('assets'));
+
+//fire controllers
+todoController(app);
+
+//listen to port
+app.listen(3000);
+console.log('you are listening on port 3000');
