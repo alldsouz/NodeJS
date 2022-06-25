49328706f07885aa44a8e4b161ef5384983d9df0
alldsouz <97952084+alldsouz@users.noreply.github.com>
2022-06-25 11:56:20 +0530

    11:45AM

--- template.js
+++ template.js
@@ -1,31 +1,20 @@
-var express = require ('express');
+var express = require('express');
 
+var app = express();
 
+app.set('view engine', 'ejs');
 
-var app = express()
-
-
-
-app.get('/', function(req, res){
-
-    res.send('this is the homepage')
-
+app.get('/',function(req,res){
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/contact', function(req, res){
-
-    res.send('this is the contactpage')
-
+app.get('/contact',function(req,res){
+    res.sendFile(__dirname + '/contact.html');
 });
 
-
-
-app.get('/profile/:name', function(req,res){
-
-    res.send('you requested to see a profile  with the name of ' + req.params.name);
-
+app.get('/profile/:name',function(req,res){
+    var data = {age:21, job:'work', hobbies: ['eating','drinking']};
+    res.render('profile',{person: req.params.name , data: data});
 });
-
-
 
 app.listen(3000);
